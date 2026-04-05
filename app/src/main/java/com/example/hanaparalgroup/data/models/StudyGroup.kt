8a175a34600d2edd03357daefb8217cfb837dbea
Chris-Channie <christiandanbalanag@gmail.com>
2026-04-06 03:10:30 +0800

    Revised the code to be able to compatible to the needed.

--- app/src/main/java/com/example/hanaparalgroup/data/models/StudyGroup.kt
+++ app/src/main/java/com/example/hanaparalgroup/data/models/StudyGroup.kt
@@ -1,10 +1,15 @@
 package com.example.hanaparalgroup.data.models
 
+import com.google.firebase.firestore.IgnoreExtraProperties
+
+@IgnoreExtraProperties
 data class StudyGroup(
-    val groupId: String = "",
-    val groupName: String = "",
+    var groupId: String = "",
+    val name: String = "",
     val description: String = "",
-    val adminId: String = "",        // Requirement #3 (Creator is admin)
-    val members: List<String> = emptyList(), // Requirement #4 (Join list)
+    val adminId: String = "",
+    val adminName: String = "",
+    val members: MutableList<String> = mutableListOf(),
+    val maxMembers: Int = 5, // Default, Cativo will override this with Remote Config
     val createdAt: Long = System.currentTimeMillis()
 )
